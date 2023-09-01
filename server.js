16e085cabd7bc54ed8feb28876a3774db653f3b9
juanes G <jgalvez98@gmail.com>
2023-08-31 22:13:31 -0400

    server js code fixed and route code typo fixed

--- server.js
+++ server.js
@@ -1,43 +1,26 @@
+// Dependencies
 const express = require('express');
-const path = require('path');//this is the way to do a local on the html route, with this we can find the path of the files
 
-const api = require('./routes/index.js');
+// app use express
+const app = express();
 
+// creating environment variable port
 const PORT = process.env.PORT || 3001;
 
-const app = express();
-//client -> middleware (pre-processing before give to the server app.use() -> server
-// Import custom middleware, "cLog"
 
-
-// Middleware for parsing JSON and urlencoded form data
-//data parser in app.use to parse client data to req.body(original client data object)
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-//middleware to modular api routes
-//modulize: breakdown  api route in separate files
-//http:/localhost:3001/api  (this is api base)
-app.use('/api', api);
-
-//middleware to make public the homepage url http://localhost:3001
+// asks express to create a route for every file in the 'public' folder and give it a '/' route
 app.use(express.static('public'));
+// sets up express app to handel data parser, middle wear created req.body
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-// GET Route for homepage
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
-);
 
-// GET Route for notes page
-app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/notes.html'))
-);
+// routes to route files
+require('./routes/apiRoutes')(app);
+require('./routes/htmlRoutes')(app);
 
-// Wildcard route to direct users to a index.html
-app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, 'public/index.html'))
-);
 
-app.listen(PORT, () =>
-  console.log(`App listening at http://localhost:${PORT}`)
-);
+// app listener - starts the server
+app.listen(PORT, () => {
+  console.log(`Server available at localhost${PORT}`);
+});
